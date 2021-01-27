f971211ba28648cbc0aec2708dc6719e2fcf2cb4
Dmitriy.Kubarev <dmitriy.kubarev@russdragmet.ru>
2021-01-27 11:07:05 +0300

    admin panel flat filter by properties is added

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ["created_at"]
     list_display = ('address', 'price', 'new_building', 'town', 'construction_year')
     list_editable = ('new_building',)
+    list_filter = ('new_building', 'has_balcony', 'rooms_number', 'floor')
 
 
 admin.site.register(Flat, FlatAdmin)
